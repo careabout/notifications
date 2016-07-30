f4c1a6a1ee45ffcba7d9d07d3b84a50eae3ba278
Don Smith <locksmithdon@gmail.com>
2016-07-31 10:50:18 +1200

    Added CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
+var cors = require('cors')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -11,6 +12,7 @@ var subscriptions = require('./routes/subscription')
 var PORT = process.env.PORT || 3000
 var app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'static')))
 app.use('/v1/subscriptions', subscriptions)
