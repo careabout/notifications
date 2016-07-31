edc55236cb7f681b8874e327850e288f8d619595
Don Smith <locksmithdon@gmail.com>
2016-07-31 16:16:30 +1200

    Retrieving decisions

--- workers/sendNotifications.js
+++ workers/sendNotifications.js
@@ -1,42 +1,64 @@
+var logger = require('logfmt')
+var request = require('superagent')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-var message = { 
-  app_id: process.env.APP_ID,
-  contents: {"en": "This is your Auckland notification."},
-  included_segments: ["Auckland"]
+getDecisions()
+
+function getDecisions () {
+  request
+    .get('https://care-about.herokuapp.com/v1/decisions/?processed=false')
+    .set('Accept', 'application/json')
+    .end((err, res) => {
+      if (err) {
+        logger.log({type: 'error', msg: err.message})
+      } else {
+        console.log(res.body.decisions)
+        sendNotifications(res.body.decisions)
+      }
+    })
 }
 
-sendNotification(message)
+function sendNotifications (decisions) {
+  // for each decision
+  decisions.forEach((decision) => {
+    var allTopics = decision.topics.concat(decision.locations)
+    var message = { 
+      app_id: process.env.APP_ID,
+      headings: {"en": allTopics[0]},
+      contents: {"en": decision.title},
+      included_segments: allTopics,
+      chrome_web_icon: process.env.SITE_URL + '/notification_logo.png',
+      firefox_icon: process.env.SITE_URL + '/notification_logo.png',
+      url: process.env.SITE_URL + '/v1/decisions/' + decision._id
+    }
+    sendNotification(decision._id, message)
+    updateDecisionAsProcessed(decision._id)
+  })
+}
 
-function sendNotification (data) {
-  var headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Basic " + process.env.AUTH_HEADER
-  }
-  
-  var options = {
-    host: "onesignal.com",
-    port: 443,
-    path: "/api/v1/notifications",
-    method: "POST",
-    headers: headers
-  }
-  
-  var https = require('https')
-  var req = https.request(options, function(res) {  
-    res.on('data', function(data) {
-      console.log("Response:")
-      console.log(JSON.parse(data))
+function sendNotification (id, message) {
+  request
+    .post('https://onesignal.com/api/v1/notifications/' + id)
+    .set('Content-Type', 'application/json')
+    .set('Authorization', 'Basic ' + process.env.AUTH_HEADER)
+    .send(message)
+    .end((err, res) => {
+      if (err) {
+        logger.log({type: 'error', msg: err.message})
+      }
     })
-  })
-  
-  req.on('error', function(e) {
-    console.log("ERROR:")
-    console.log(e)
-  })
-  
-  req.write(JSON.stringify(data))
-  req.end()
+}
+
+function updateDecisionAsProcessed (id) {
+  request
+    .put('https://care-about.herokuapp.com/v1/decisions/' + id + '/processed')
+    .set('Accept', 'application/json')
+    .end((err, res) => {
+      if (err) {
+        logger.log({type: 'error', msg: err.message})
+      }
+    })
 }
